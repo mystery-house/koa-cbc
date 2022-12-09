e1ced4735c674801c95e2562ea9ca2e0ee5feb70
Andy Chase <andychase@gmail.com>
2022-12-08 21:18:00 -0500

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
-import { Context } from "koa";
-
+import { Context, HttpError, Next } from "koa";
 interface Indexable {
   [key: string]: any;
 }
 
+export const HttpMethods = ["get", "post", "put", "patch", "delete", "head", "connect", "options", "trace"] as const
+
 /**
- * An error object conforming to the
- * [JSend](https://github.com/omniti-labs/jsend) specification
+ * Valid HTTP methods
  */
-type JSendError = {
-  code?: number;
-  status: "error" | "fail";
-  message: string;
-  data?: unknown;
-}
+export type HttpMethod = typeof HttpMethods[number]
 
 /** 
  * Valid HTTP status codes 
@@ -23,132 +18,138 @@ export type HttpStatusCode = 100 | 101 | 102 | 103 | 104 | 200 | 201 | 202 | 203
 /**
  * Describes the expected interface of a Controller class
  */
-export interface ControllerInterface {
-  ctx: Context
-  dispatch: () => void;
-  get?: () => void;
-  post?: () => void;
-  put?: () => void;
-  patch?: () => void;
-  delete?: () => void;
-  head?: () => void;
-  connect?: () => void;
-  options?: () => void;
-  trace?: () => void;
+interface BaseController {
+  /** Handles HTTP GET requests */
+  get?(): Promise<any>;
+  post?(): Promise<any>;
+  put?(): Promise<any>;
+  patch?(): Promise<any>;
+  delete?(): Promise<any>;
+  head?(): Promise<any>;
+  connect?(): Promise<any>;
+  options?(): Promise<any>;
+  trace?(): Promise<any>;
 }
 
 /**
- * Valid HTTP methods
- */
-export type HttpMethod = "get" | "post" | "put" | "patch" | "delete" | "head" | "connect" | "options" | "trace"
-
-/**
- * `Controller` is an abstract base class that can be extended to handle
+ * `BaseController` is an abstract base class that can be extended to handle
  * requests for a given resource, based on requests. The constructor takes a
  * Koa `Context` object and the `dispatch` method invokes the appropriate 
  * class method indicated by the context's `request` object.
+ * 
+ * If a `next` function is passed to the constructor, it can be explicitly 
+ * called internally by using the private `_next` method. Otherwise, it will
+ * be called at the end of the `dispatch` method.
  */
-export default abstract class Controller implements ControllerInterface {
+abstract class BaseController {
 
   /** The Koa `Context` object  */
-  ctx: Context
+  ctx: Context;
+  _next?: Next;
+  _nextCalled: boolean = false;
 
-  constructor(ctx: Context) {
-    if (this.constructor == Controller) {
+  constructor(ctx: Context, next?: Next) {
+    if (this.constructor == BaseController) {
       throw new Error(
-        "Controller is an abstract class and cannot be instantiated."
+        "Controller is an abstract class and should not be instantiated."
       );
     }
     this.ctx = ctx;
-    const method = ctx.request.method.toLowerCase();
-
+    if (next) this._next = next;
   }
 
   /**
-   * Dispatches the controller method designated by the context request.
+   * Dispatches the controller method designated by the context request and sets the response
+   * body accordingly.
+   * 
+   * Because the most typical pattern is "call a method, send a response", methods return just the
+   * body value, and any changes to the HTTP status code or headers are left up to the implementor.
+   * 
+   * The default response status code is 200.
    */
-  dispatch() {
+  async dispatch() {
     const methodName = this.ctx.request.method.toLowerCase() as HttpMethod;
+
+    if (HttpMethods.indexOf(methodName) === -1) {
+      this.ctx.throw(400, `Invalid request method: ${methodName.toUpperCase()}`)
+    }
+
+    if (!(this as Indexable)[methodName]) {
+      this.ctx.throw(501, `${methodName.toUpperCase()} method not implemented`)
+    }
+
     try {
       // Hat-tip to Daniel W Strimpel for this solution
       // for making method names dynamically indexable. (https://stackoverflow.com/a/53194405)
-      (this as Indexable)[methodName]()
+      const body = await (this as Indexable)[methodName]();
+      this.setResponseBody(body);
+      this.next();
     }
     catch (error) {
-      const message = error instanceof Error ? error.message : String(error)
-      this.error(501, `Could not dispatch ${methodName.toUpperCase()}: ${message}`)
+      let message = "An error occurred";
+      let code = 500;
+
+      if (error instanceof Error) {
+        message = error.message;
+      }
+      if (error instanceof HttpError) {
+        code = error.code;
+      }
+      this.ctx.throw(code, message);
     }
   }
 
   /**
-   * Handles HTTP `GET` requests.
+   * Invokes the middleware `next` function, if set.
    */
-  get() {
-    throw new Error("Method not implemented");
+  async next() {
+    if (this._next) {
+      await this._next();
+      this._nextCalled = true;
+    }
   }
 
   /**
-   * Handles HTTP `POST` requests.
+   * Sets the response HTTP status code
+   * @param status_code 
    */
-  post() {
-    throw new Error("Method not implemented");
+  setResponseStatus(status_code: HttpStatusCode) {
+    this.ctx.status = status_code;
   }
 
   /**
-   * Handles HTTP `PUT` requests.
+   * Merges custom headers with any existing context response headers
+   * @param headers 
    */
-  put() {
-    throw new Error("Method not implemented");
+  setResponseHeaders(headers: { [key: string]: string | string[] }) {
+    const keys = Object.keys(headers);
+    for (let i = 0; i < keys.length; i++) {
+      this.ctx.set(keys[i], headers[keys[i]]);
+    }
   }
 
   /**
-   * Handles HTTP `PATCH` requests.
+   * Sets the response body.
    */
-  patch() {
-    this.put();
+  setResponseBody(body: any) {
+    this.ctx.body = body
   }
 
   /**
-   * Handles HTTP `DELETE` requests.
-   */
-  delete() {
-    throw new Error("Method not implemented");
-  }
-
-  /**
-   * Handles HTTP `HEAD` requests.
-   */
-  head() {
-    throw new Error("Method not implemented");
-  }
-
-  /**
-   * Handles HTTP `CONNECT` requests.
-   */
-  connect() {
-    throw new Error("Method not implemented");
-  }
-
-  /**
-   * Handles HTTP `OPTIONS` requests.
-   */
-  options() {
-    throw new Error("Method not implemented");
-  }
-
-  /**
-   * Sets the context response status code and message. The message is formatted as a
-   * [JSEND](https://github.com/omniti-labs/jsend) response.
-   *
+   * Throws an error using the context object. 
+   * 
+   * Note that these are user-level errors, and the message 
+   * may be exposed to the client.
+   * 
    * @param status_code 
    * @param message 
    */
-  private error(status_code: HttpStatusCode, message: string) {
-    this.ctx.status = status_code;
-    this.ctx.body = {
+  error(status_code: HttpStatusCode, message: string) {
+    this.ctx.throw({
       code: status_code,
-      status: "error",
-      message: message,
-    } as JSendError;
+      message: message
+    });
   }
 }
+
+export default BaseController;
